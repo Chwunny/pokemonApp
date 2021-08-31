9cc730a9caab66f4052d23c8aec3edbc44781d4f
Zach King <zacheking10@gmail.com>
2021-08-31 13:56:19 -0600

    added rollbar html check

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path")
-
+const Rollbar = require('rollbar')
 const app = express();
 
+let rollbar = new Rollbar({
+  accessToken: "66b9f4b729b54f6a9e36d77a4aff0c62",
+  captureUncaught: true,
+  captureUnhandledRejections: true
+})
 
 app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'))
+  rollbar.info('html file served successfully')
 })
 
 app.use("/css", express.static(path.join(__dirname, '../client/styles.css')))
