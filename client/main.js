ff60465910b6b06bc69ff0b249c6d2efb680b6f1
Zach King <zacheking10@gmail.com>
2021-09-07 16:44:25 -0600

    tuesday progress, added ALOT, background, beta functionality, positioning

--- client/main.js
+++ client/main.js
@@ -3,8 +3,11 @@ Rollbar.info("Rollbar on the front end is working")
 
 let bestPokemonIds = [384, 151, 493, 38, 143, 157, 245, 25, 83, 135]
 
-const form = document.querySelector('form')
+const form = document.getElementById('form1')
 const searchInput = document.getElementById('pokemonSearch')
+
+const form2 = document.getElementById('form2')
+const battleSearchInput = document.getElementById('pokemonBattleSearch')
 
 const types = {
     "bug" : "/photos/BugIC.png",
@@ -27,6 +30,26 @@ const types = {
     "water" : "/photos/WaterIC.png"
 }
 
+const weakness = {
+    "bug" : ["flying", "rock", "fire"],
+    "dark" : ["fighting", "bug", "fairy"],
+    "dragon" : ["ice", "dragon", "fairy"],
+    "electric" : ["ground"],
+    "fairy" : ["posion", "steel"],
+    "fighting" : ["flying", "psychic", "fairy"],
+    "fire" : ["ground", "rock", "water"],
+    "flying" : ["rock", "steel", "electric"],
+    "ghost" : ["ghost", "dark"],
+    "grass" : ["flying", "posion", "bug", "fire", "ice"],
+    "ground" : ["water", "grass", "ice"],
+    "ice" : ["fighting", "rock", "steel", "fire"],
+    "normal" : ["fighting"],
+    "poison" : ["ground", "psychic"],
+    "psychic" : ["bug", "ghost", "dark"],
+    "rock" : ["fighting", "ground", "steel", "water", "grass"],
+    "steel" : ["fighting", "ground", "fire"],
+    "water" : ["grass", "electric"]
+}
 
 const randomPokemon = () => {
     try {
@@ -145,8 +168,92 @@ const submitHandler = (e) => {
     
 }
 
+/// Pokemon battle section
+let opponentType = '' 
+let yourType = ''
+
+const opponentPokemon = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 898)}`)
+        .then(res => {
+            // console.log(res.data)
+            let name = res.data.species.name
+            name = name.charAt(0).toUpperCase() + name.slice(1)
+            opponentType = res.data.types[0].type.name
+
+            let spriteImg = res.data.sprites.front_default
+
+            document.getElementById('opponentNameBox').textContent = name
+            document.getElementById('opponentPokemon').src = spriteImg
+            
+        })
+    }
+
+
+const yourRandomPokemon = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 898)}`)
+    .then(res => {
+        let name = res.data.species.name
+        name = name.charAt(0).toUpperCase() + name.slice(1)
+        yourType = res.data.types[0].type.name
+        
+        let spriteImg = res.data.sprites.back_default
+
+        document.getElementById('yourPokemonNameBox').textContent = name
+        document.getElementById('yourPokemon').src = spriteImg
+        document.getElementById('queryText').textContent = `What will ${name} do?`
+
+    })
+}
+
+
+const battleSubmitHandler = (e) => {
+    e.preventDefault()
+
+    if (battleSearchInput.value.length > 0){
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${battleSearchInput.value}`)
+        .then(res => {
+            let name = res.data.species.name
+            name = name.charAt(0).toUpperCase() + name.slice(1)
+            yourType = res.data.types[0].type.name
+        
+            let spriteImg = res.data.sprites.back_default
+
+            document.getElementById('yourPokemonNameBox').textContent = name
+            document.getElementById('yourPokemon').src = spriteImg
+            document.getElementById('queryText').textContent = `What will ${name} do?`
+            
+        })
+    }
+    battleSearchInput.value = ''
+}
+
+
+const startFight = () => {
+//   console.log("opponent type:", opponentType, "weakness:", weakness[opponentType])
+//   console.log("your type:", yourType, "weakness:", weakness[yourType])
+
+  if (weakness[opponentType].includes(yourType)){
+    console.log('You Win!')
+    // return
+  } else if (weakness[yourType].includes(opponentType)){
+      console.log('You Lose!')
+    //   return
+  } else {
+      console.log('You Win!')
+    //   return
+  }
+
+}
+
 document.getElementById('randomPokemon').addEventListener('click', randomPokemon)
 document.getElementById('bestPokemon').addEventListener('click', bestPokemon)
 document.getElementById('shinyButton').addEventListener('click', getShiny)
 document.getElementById('defaultButton').addEventListener('click', getDefault)
 form.addEventListener('submit', submitHandler)
+
+/// Battle Section
+
+document.getElementById('opponentPokemonBtn').addEventListener('click', opponentPokemon)
+document.getElementById('pokemonBattleRandomBtn').addEventListener('click', yourRandomPokemon)
+form2.addEventListener('submit', battleSubmitHandler)
+document.getElementById('fightBtn').addEventListener('click', startFight)
